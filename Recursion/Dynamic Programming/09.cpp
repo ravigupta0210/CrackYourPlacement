915e800fe4ce36ef2a5386cef742fb6dd4cd2283
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2023-10-03 08:51:37 +0530

    Create 09.cpp

---
+++ Recursion/Dynamic Programming/09.cpp
@@ -0,0 +1,49 @@
+// Ninja And The Fence
+
+
+#include <bits/stdc++.h> 
+#define MOD 1000000007
+int add(int a,int b){
+    return ((a%MOD) +( b%MOD))%MOD;
+}
+
+int mul(int a,int b){
+    return ((a%MOD)* 1LL * ( b%MOD))%MOD;
+}
+
+int solve(int n,int k,vector<long long int> &dp){
+    if(n==1) return k;
+    if(n==2) return add(k,mul(k,k-1));
+    if(dp[n]!=-1) return dp[n];
+    return dp[n]=add(mul(solve(n-2,k,dp),k-1),mul(solve(n-1,k,dp),k-1));
+}
+
+int solveTab(int n,int k){
+    vector<long long int> dp(n+1,0);
+    dp[1]=k;
+    dp[2]=add(k,mul(k,k-1));
+    for(int i=3;i<=n;i++){
+        dp[i]=add(mul(dp[i-2],k-1),mul(dp[i-1],k-1));
+    }
+    
+    return dp[n];
+}
+
+int solveSO(int n,int k){
+    
+    int prev2=k;
+    int prev1=add(k,mul(k,k-1));
+    for(int i=3;i<=n;i++){
+        int ans=add(mul(prev2,k-1),mul(prev1,k-1));
+        prev2 = prev1;
+        prev1 = ans;
+    }
+    
+    return prev1;
+}
+
+int numberOfWays(int n, int k) {
+    // vector<long long int> dp(n+1,-1);
+    // return solve(n,k,dp);
+    return solveSO(n,k);
+}
