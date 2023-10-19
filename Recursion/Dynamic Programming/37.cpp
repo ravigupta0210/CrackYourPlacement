98e977690bcb0f2ca19c5aa601be5c0fb16b6893
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2023-10-19 10:23:42 +0530

    Create 37.cpp

---
+++ Recursion/Dynamic Programming/37.cpp
@@ -0,0 +1,79 @@
+// Edit Distance
+
+
+
+class Solution {
+public:
+int solve(string a,string b,int i,int j,vector<vector<int>> &dp){
+    if(i == a.length()) return b.length()-j;
+    if(j==b.length()) return a.length()-i;
+    if(dp[i][j]!=-1) return dp[i][j];
+    int ans = 0;
+    if(a[i] == b[j])
+    ans = solve(a,b,i+1,j+1,dp);
+    else{
+        int insertAns = 1+solve(a,b,i,j+1,dp);
+        int deleteAns = 1+solve(a,b,i+1,j,dp);
+        int replaceAns = 1+solve(a,b,i+1,j+1,dp);
+        ans = min(insertAns,min(deleteAns,replaceAns));
+    }
+    return dp[i][j] = ans;
+}
+int solveTab(string a,string b){
+    vector<vector<int>> dp(a.length()+1,vector<int>(b.length()+1,0));
+    for(int j=0;j<b.length();j++)
+    dp[a.length()][j] = b.length()-j;
+    for(int i=0;i<a.length();i++)
+    dp[i][b.length()] = a.length()-i;
+
+    for(int i=a.length()-1;i>=0;i--){
+        for(int j=b.length()-1;j>=0;j--){
+            int ans = 0;
+            if(a[i] == b[j])
+            ans = dp[i+1][j+1];
+            else{
+                int insertAns = 1+dp[i][j+1];
+                int deleteAns = 1+dp[i+1][j];
+                int replaceAns = 1+dp[i+1][j+1];
+                ans = min(insertAns,min(deleteAns,replaceAns));
+            }
+            dp[i][j] = ans;
+        }
+    }
+    return dp[0][0];
+}
+int solveTab2(string a,string b){
+    // vector<vector<int>> dp(a.length()+1,vector<int>(b.length()+1,0));
+    vector<int> curr(b.length()+1,0);
+    vector<int> next(b.length()+1,0);
+
+    for(int j=0;j<b.length();j++)
+    next[j] = b.length()-j;
+   
+
+    for(int i=a.length()-1;i>=0;i--){
+        for(int j=b.length()-1;j>=0;j--){
+            curr[b.length()] = a.length()-i;
+            int ans = 0;
+            if(a[i] == b[j])
+            ans = next[j+1];
+            else{
+                int insertAns = 1+curr[j+1];
+                int deleteAns = 1+next[j];
+                int replaceAns = 1+next[j+1];
+                ans = min(insertAns,min(deleteAns,replaceAns));
+            }
+            curr[j] = ans;
+        }
+        next = curr;
+    }
+    return curr[0];
+}
+    int minDistance(string word1, string word2) {
+        // vector<vector<int>> dp(word1.length(),vector<int>(word2.length(),-1));
+        // return solve(word1,word2,0,0,dp);
+        if(word1.length()==0) return word2.length();
+        if(word2.length()==0) return word1.length();
+        return solveTab2(word1,word2);
+    }
+};
