2b098109aa48c108e03ce9dd687445935ca2636e
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2023-12-30 12:16:19 +0530

    Update 30_Dec.cpp

--- Daily/30_Dec.cpp
+++ Daily/30_Dec.cpp
@@ -16,3 +16,37 @@ public:
        return res;
     }
 };
+
+
+
+// Maximum Binary Tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+TreeNode* solve(vector<int> &nums,int start,int end){
+    if(start > end) return NULL;
+    int p =start;
+    for(int i=p+1;i<=end;i++){
+        if(nums[i]>nums[p])
+        p=i;
+    }
+    TreeNode* root = new TreeNode(nums[p]);
+    root->left = solve(nums,start,p-1);
+    root->right = solve(nums,p+1,end);
+    return root;
+}
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        return solve(nums,0,nums.size()-1);
+    }
+};
