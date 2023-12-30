d4e14cbd43c6285fa08e14e18d70503ab498799b
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2023-12-30 12:16:46 +0530

    Update 30_Dec.cpp

--- Daily/30_Dec.cpp
+++ Daily/30_Dec.cpp
@@ -50,3 +50,72 @@ TreeNode* solve(vector<int> &nums,int start,int end){
         return solve(nums,0,nums.size()-1);
     }
 };
+
+
+// Difference Between Ones and Zeros in Row and Column
+
+class Solution {
+public:
+int solve1(vector<vector<int>>& grid,int index,int n){
+    int ans = 0;
+    for(int i=0;i<n;i++){
+        if(grid[index][i] == 1)
+        ans++;
+    }
+    return ans;
+}
+int solve2(vector<vector<int>>& grid,int index,int n){
+    int ans = 0;
+    for(int i=0;i<n;i++){
+        if(grid[i][index] == 1)
+        ans++;
+    }
+    return ans;
+}
+int solve3(vector<vector<int>>& grid,int index,int n){
+    int ans = 0;
+    for(int i=0;i<n;i++){
+        if(grid[index][i] == 0)
+        ans++;
+    }
+    return ans;
+}
+int solve4(vector<vector<int>>& grid,int index,int n){
+    int ans = 0;
+    for(int i=0;i<n;i++){
+        if(grid[i][index] == 0)
+        ans++;
+    }
+    return ans;
+}
+    vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        // vector<vector<int>> ans( m , vector<int> (n, 0));
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         int oneRow = solve1(grid,i,n);
+        //         int oneCol = solve2(grid,j,m);
+        //         int zeroRow = solve3(grid,i,n);
+        //         int zeroCol = solve4(grid,j,m);
+        //         int temp = oneRow+oneCol - zeroRow - zeroCol;
+        //         ans[i][j] = temp;
+        //     }
+        // }
+        // return ans;
+        vector<int> row(m);
+        vector<int> col(n);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                row[i] += grid[i][j];
+                col[j] += grid[i][j];
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                grid[i][j] = 2*(row[i]+col[j]) - m- n;
+            }
+        }
+        return grid;
+    }
+};
