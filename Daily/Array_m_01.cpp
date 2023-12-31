a9bc238df9b76a6b24cb206f7a5e826aa41f344f
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2023-12-31 12:13:30 +0530

    Update Array_m_01.cpp

--- Daily/Array_m_01.cpp
+++ Daily/Array_m_01.cpp
@@ -13,3 +13,40 @@ public:
         return head;
     }
 };
+
+
+// Queries on a Permutation With Key
+
+class Solution {
+public:
+int findSolve(int key,vector<int> p){
+    for(int i=0;i<p.size();i++){
+        if(key == p[i]){
+            return i;
+        }
+    }
+    return 0;
+}
+vector<int> refactor(vector<int> p,int key){
+    vector<int> temp ;
+    temp.push_back(key);
+    for(int i=0;i<p.size();i++){
+        if(key != p[i])
+        temp.push_back(p[i]);
+    }
+    return temp;
+}
+    vector<int> processQueries(vector<int>& queries, int m) {
+        vector<int> p;
+        for(int i=1;i<=m;i++)
+        p.push_back(i);
+
+        vector<int> ans;
+        for(int i=0;i<queries.size();i++){
+            int index = findSolve(queries[i],p);
+            ans.push_back(index);
+            p = refactor(p,queries[i]);
+        }
+        return ans;
+    }
+};
