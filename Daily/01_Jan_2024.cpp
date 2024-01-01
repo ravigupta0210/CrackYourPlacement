06327bafbc41c45b2894eaf2d71db35f18869cc4
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-01 12:32:20 +0530

    Update 01_Jan_2024.cpp

--- Daily/01_Jan_2024.cpp
+++ Daily/01_Jan_2024.cpp
@@ -45,3 +45,28 @@ public:
         return maxi;
     }
 };
+
+
+// Number of Laser Beams in a Bank
+
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        if(bank.size()==1) return 0;
+        vector<int> once;
+        int count = 0;
+        for(auto i:bank){
+            count=0;
+            for(auto j:i){
+                if(j=='1') count++;
+            }
+            if(count) once.push_back(count);
+        }
+        if(once.size()<=1) return 0;
+        int ans = 0;
+        for(int i=0;i<once.size()-1;i++){
+            ans+=once[i]*once[i+1];
+        }
+        return ans;
+    }
+};
