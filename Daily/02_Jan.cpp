fc103358b9d0d48a13c31097b3d46ae62a528f78
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-02 10:43:57 +0530

    Update 02_Jan.cpp

--- Daily/02_Jan.cpp
+++ Daily/02_Jan.cpp
@@ -66,3 +66,26 @@ public:
         return ans;
     }
 };
+
+// Partition Labels
+
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        unordered_map<char,int> mp;
+        for(int i=0;i<s.size();i++)
+        mp[s[i]] = i;
+        int j=0;
+        int maxi = 0;
+        vector<int> ans;
+        for(int i=0;i<s.size();i++){
+            maxi = max(maxi,mp[s[i]]);
+            if(i==maxi){
+                ans.push_back(i-j+1);
+                j=i+1;
+            }
+        }
+        return ans;
+
+    }
+};
