6be81b766bf78149632ea4410ac97d97da23ca99
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-02 10:43:31 +0530

    Update 02_Jan.cpp

--- Daily/02_Jan.cpp
+++ Daily/02_Jan.cpp
@@ -34,3 +34,35 @@ public:
 // Your Solution object will be instantiated and called as such:
 // Solution solution;
 // solution.decode(solution.encode(url));
+
+
+// Execution of All Suffix Instructions Staying in a Grid
+
+class Solution {
+public:
+    vector<int> executeInstructions(int n, vector<int>& startPos, string s) {
+        vector<int> ans;
+        int count = 0;
+        int row,col;
+        for(int i=0;i<s.length();i++){
+            count = 0;
+            row = startPos[0];
+            col = startPos[1];
+            for(int j=i;j<s.length();j++){
+                char ch = s[j];
+                if(ch == 'L')
+                col--;
+                else if(ch == 'R')
+                col++;
+                else if(ch == 'U')
+                row--;
+                else row++;
+                if(row >= 0 && row <= n-1 && col >=0 && col<= n-1)
+                count++;
+                else break;
+            }
+            ans.push_back(count);
+        }
+        return ans;
+    }
+};
