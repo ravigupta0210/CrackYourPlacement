c4c36ffc6f5e54f9b4390cef744590862f58e90e
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-03 14:06:38 +0530

    Update 03_Jan.cpp

--- Daily/03_Jan.cpp
+++ Daily/03_Jan.cpp
@@ -50,3 +50,16 @@ string change(string word){
         return ans;
     }
 };
+
+
+// Remove All Occurrences of a Substring
+
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while(s.length()!=0 && s.find(part) < s.length()){
+            s.erase(s.find(part),part.length());
+        }
+        return s;
+    }
+};
