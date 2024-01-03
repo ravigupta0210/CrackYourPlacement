ba5b9d053b26bcb57432b6b8be129f020e1ceb60
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-03 14:06:14 +0530

    Update 03_Jan.cpp

--- Daily/03_Jan.cpp
+++ Daily/03_Jan.cpp
@@ -19,3 +19,34 @@ public:
         return s.length()-count;
     }
 };
+
+
+// Find and Replace Pattern
+
+
+class Solution {
+public:
+string change(string word){
+    unordered_map<char,char> mp;
+    char ch = 'a';
+    for(int i=0;i<word.length();i++){
+        if(mp.find(word[i]) == mp.end()){
+            mp[word[i]] = ch;
+            ch++;
+        }
+    }
+    for(int i=0;i<word.length();i++){
+        word[i] = mp[word[i]];
+    }
+    return word;
+}
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        pattern = change(pattern);
+        vector<string> ans;
+        for(auto word: words){
+            if(pattern == change(word))
+            ans.push_back(word);
+        }
+        return ans;
+    }
+};
