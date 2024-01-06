24fa78a9d3d04ef2a93ebe75e1fdb2ce3965af3f
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-06 11:32:31 +0530

    Update 06_Jan.cpp

--- Daily/06_Jan.cpp
+++ Daily/06_Jan.cpp
@@ -93,3 +93,29 @@ public:
     return count;
     }
 };
+
+
+// Letter Case Permutation
+
+class Solution {
+public:
+void solve(string &s,int i,vector<string> &ans){
+    if(i==s.size()){
+        ans.push_back(s);
+        return ;
+    }
+    char c = s[i];
+    s[i] = islower(c)?toupper(c):tolower(c);
+    solve(s,i+1,ans);
+    if(isalpha(c)){
+        s[i] = c;
+        solve(s,i+1,ans);
+    }
+}
+
+    vector<string> letterCasePermutation(string s) {
+        vector<string> ans;
+        solve(s,0,ans);
+        return ans;
+    }
+};
