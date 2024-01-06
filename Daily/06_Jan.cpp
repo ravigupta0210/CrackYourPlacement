3fff9656c10662c83436d2d71247083c122dd495
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-06 11:31:41 +0530

    Update 06_Jan.cpp

--- Daily/06_Jan.cpp
+++ Daily/06_Jan.cpp
@@ -35,3 +35,40 @@ unordered_map<string,pair<double,int>> mp2;
  * obj->checkOut(id,stationName,t);
  * double param_3 = obj->getAverageTime(startStation,endStation);
  */
+
+
+
+// Display Table of Food Orders in a Restaurant
+
+
+class Solution {
+public:
+    vector<vector<string>> displayTable(vector<vector<string>>& orders) {
+        map<pair<string,string>,int> food;
+        set<int> tables;
+        set<string> foodItems;
+        for(auto order:orders){
+            tables.insert(stoi(order[1]));
+            foodItems.insert(order[2]);
+            pair<string,string> temp = {order[2],order[1]};
+            food[temp]++;
+        }
+        vector<string> header;
+        header.push_back("Table");
+        for(auto foodi : foodItems)
+        header.push_back(foodi);
+
+        vector<vector<string>> ans;
+        ans.push_back(header);
+        for(auto table:tables){
+            vector<string> v;
+            v.push_back(to_string(table));
+            for(int i=1;i<ans[0].size();i++){
+                string name = ans[0][i];
+                v.push_back(to_string(food[{name,to_string(table)}]));
+            }
+            ans.push_back(v);
+        }
+        return ans;
+    }
+};
