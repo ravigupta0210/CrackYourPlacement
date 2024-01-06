0318620ca5ac967464a2703c1ca8084011399ea8
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-06 11:32:10 +0530

    Update 06_Jan.cpp

--- Daily/06_Jan.cpp
+++ Daily/06_Jan.cpp
@@ -72,3 +72,24 @@ public:
         return ans;
     }
 };
+
+
+
+// Number of Pairs of Strings With Concatenation Equal to Target
+
+class Solution {
+public:
+    int numOfPairs(vector<string>& nums, string target) {
+        int count = 0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<nums.size();j++){
+                if(i!=j){
+                    string temp = nums[i]+nums[j];
+                    if(temp == target)
+                    count++;
+                }
+            }
+        }
+    return count;
+    }
+};
