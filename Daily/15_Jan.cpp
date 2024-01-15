bd22f854015d774827381dde276a9a718bc0c1f6
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-15 10:56:55 +0530

    Update 15_Jan.cpp

--- Daily/15_Jan.cpp
+++ Daily/15_Jan.cpp
@@ -69,3 +69,20 @@ public:
         }
     }
 };
+
+
+// Maximum Subarray
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = INT_MIN;int temp=0;
+        for(int i=0;i<nums.size();i++){
+            temp+=nums[i];
+            sum=max(sum,temp);
+            if(temp<0) temp=0;
+        }
+        return sum;
+    }
+};
