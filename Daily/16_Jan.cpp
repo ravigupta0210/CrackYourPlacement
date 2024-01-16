f61639fc4f8a826e1c97614e340129a02137c9cc
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-16 10:14:22 +0530

    Update 16_Jan.cpp

--- Daily/16_Jan.cpp
+++ Daily/16_Jan.cpp
@@ -90,3 +90,18 @@ vector<int> Solution::repeatedNumber(const vector<int> &A) {
     }
     return {first,second};
 }
+
+
+//  Count Inversions
+
+#include <bits/stdc++.h> 
+long long getInversions(long long *arr, int n){
+    long long ans = 0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]>arr[j])
+            ans++;
+        }
+    }
+    return ans;
+}
