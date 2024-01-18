19f567f4ff919bbdefc2ebe48f808ecd4767cfa9
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-18 22:37:22 +0530

    Update 18_Jan.cpp

--- Daily/18_Jan.cpp
+++ Daily/18_Jan.cpp
@@ -70,3 +70,20 @@ public:
         return max(maxi,cnt);
     }
 };
+
+
+// Subarray with given XOR
+
+int Solution::solve(vector<int> &A, int B) {
+   int ans=0, count=0;
+unordered_map<int,int>mp;
+for(int i=0;i<A.size();i++){
+ans^=A[i];
+if(ans==B) count++;
+if(mp.find(ans^B)!=mp.end()){
+count+=mp[ans^B];
+}
+mp[ans]++;
+}
+return count;
+}
