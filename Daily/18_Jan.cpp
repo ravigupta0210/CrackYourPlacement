b3ba34fbdb4ee6d9e27c7338b49061e8d9b377be
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-18 22:36:35 +0530

    Update 18_Jan.cpp

--- Daily/18_Jan.cpp
+++ Daily/18_Jan.cpp
@@ -12,3 +12,37 @@ public:
         return {};
     }
 };
+
+// 4Sum
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        set<vector<int>> st;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-3;i++){
+            for(int j=i+1;j<n-2;j++){
+                long long newTarget = (long long)target - (long long)nums[i]-(long long)nums[j];
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    long long sum = nums[k]+nums[l];
+                    if(sum == newTarget)
+                    {
+                        st.insert({nums[i],nums[j],nums[k],nums[l]});
+                        k++;
+                        l--;
+                    }
+                    else if(sum < newTarget)
+                    k++;
+                    else l--;
+                }
+            }
+        }
+        for(auto i:st)
+        ans.push_back(i);
+        return ans;
+    }
+};
