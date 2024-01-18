86f0e9f820a8be46c0b634ee40d4c0a29999debb
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-18 22:36:58 +0530

    Update 18_Jan.cpp

--- Daily/18_Jan.cpp
+++ Daily/18_Jan.cpp
@@ -46,3 +46,27 @@ public:
         return ans;
     }
 };
+
+
+// Longest Consecutive Sequence
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0) return 0;
+        int cnt = 1,maxi=0;
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<n;i++){
+            if(nums[i]!=nums[i-1]){
+                if(nums[i]==nums[i-1]+1)
+                cnt++;
+                else{
+                    maxi=max(maxi,cnt);
+                    cnt=1;
+                }
+            }
+        }
+        return max(maxi,cnt);
+    }
+};
