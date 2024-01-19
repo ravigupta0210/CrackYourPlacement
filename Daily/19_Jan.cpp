8f12afea665662d0ce64ccc57d7205a3a8fe92db
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-19 12:05:20 +0530

    Update 19_Jan.cpp

--- Daily/19_Jan.cpp
+++ Daily/19_Jan.cpp
@@ -25,3 +25,31 @@ public:
         return prev;
     }
 };
+
+
+// Middle of the Linked List
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* fast=head;
+        ListNode* slow = head;
+        while(fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next;
+            if(fast->next!=NULL)
+            fast=fast->next;
+        }
+        return slow;
+    }
+};
