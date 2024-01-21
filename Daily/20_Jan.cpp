30c1085485cc5c03a55fcea6db60a78a7a925958
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-21 11:05:31 +0530

    Update 20_Jan.cpp

--- Daily/20_Jan.cpp
+++ Daily/20_Jan.cpp
@@ -114,3 +114,39 @@ int solve(ListNode* head){
         return prev;
     }
 };
+
+
+// Palindrome Linked List
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int> st;
+        ListNode* temp=head;
+        while(temp){
+            st.push(temp->val);
+            temp=temp->next;
+        }
+        temp=head;
+        bool res=false;
+        while(temp){
+            if(temp->val == st.top()){
+                res=true;
+                temp=temp->next;
+                st.pop();
+            }
+            else return false;
+        }
+        return res;
+    }
+};
