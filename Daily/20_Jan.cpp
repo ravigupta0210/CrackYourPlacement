1fec666b2bfac8ddb0433c6e3295b7887a19927b
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-21 11:04:09 +0530

    Create 20_Jan.cpp

---
+++ Daily/20_Jan.cpp
@@ -0,0 +1,44 @@
+// Intersection of Two Linked Lists
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+int solve(ListNode* head){
+    int len=0;
+    while(head){
+        head=head->next;
+        len++;
+    }
+    return len;
+}
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(!headA || !headB) return NULL;
+        int lenA = solve(headA);
+        int lenB = solve(headB);
+        if(lenA > lenB){
+            while(lenA>lenB){
+                headA=headA->next;
+                lenA--;
+            }
+        }
+        else if(lenB > lenA){
+            while(lenB>lenA){
+                headB=headB->next;
+                lenB--;
+            }
+        }
+        while(headA && headB){
+            if(headA==headB) return headA;
+            headA=headA->next;
+            headB=headB->next;
+        }
+        return NULL;
+    }
+};
