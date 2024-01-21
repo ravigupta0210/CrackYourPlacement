8b94e45bb047ce64fffd9e3164a48182f636b61f
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-21 11:06:26 +0530

    Update 20_Jan.cpp

--- Daily/20_Jan.cpp
+++ Daily/20_Jan.cpp
@@ -190,3 +190,43 @@ public:
         return slow;
     }
 };
+
+
+// Flattening a Linked List
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast!=NULL)
+        {
+            fast=fast->next;
+            if(fast!=NULL){
+            fast=fast->next;
+            slow=slow->next;
+            }
+            
+            if(fast==slow){
+                break;
+            }
+
+        }
+        if(fast==NULL) return NULL;
+        slow=head;
+        while(slow!=fast){
+            slow=slow->next;
+            fast=fast->next;
+        }
+        return slow;
+    }
+};
