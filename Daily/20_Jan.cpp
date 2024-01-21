a88ef674dd008846faf87f8bc4b17b7fea7dfebe
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-21 11:05:07 +0530

    Update 20_Jan.cpp

--- Daily/20_Jan.cpp
+++ Daily/20_Jan.cpp
@@ -69,3 +69,48 @@ public:
         return false;
     }
 };
+
+
+// Reverse Nodes in k-Group
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+int solve(ListNode* head){
+    int len=0;
+    while(head){
+        head=head->next;
+        len++;
+    }
+    return len;
+}
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(!head) return NULL;
+        int n=solve(head);
+        if(n<k) return head;
+        ListNode* curr=head;
+        ListNode* prev=NULL;
+        ListNode* next=NULL;
+        int count=0;
+        while(count<k && curr!=NULL){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+            count++;
+        }
+        if(next!=NULL)
+        head->next=reverseKGroup(curr,k);
+        
+        return prev;
+    }
+};
