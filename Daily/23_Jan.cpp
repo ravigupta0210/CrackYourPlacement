b26317ebb518494461357f81b00a86c6fe5d11f0
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-23 12:45:09 +0530

    Update 23_Jan.cpp

--- Daily/23_Jan.cpp
+++ Daily/23_Jan.cpp
@@ -119,3 +119,19 @@ public:
         return ans;
     }
 };
+
+
+// Remove Duplicates from Sorted Array
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        for(int j=1;j<nums.size();j++){
+            if(nums[i]!=nums[j])
+            i++;
+            nums[i]=nums[j];
+        }
+        return i+1;
+    }
+};
