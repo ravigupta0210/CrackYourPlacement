1de1dc6e21045b61d96d523380cc5f76b267799b
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-23 12:43:22 +0530

    Update 23_Jan.cpp

--- Daily/23_Jan.cpp
+++ Daily/23_Jan.cpp
@@ -39,3 +39,36 @@ int solve(ListNode* head){
         return head;
     }
 };
+
+
+// Copy List with Random Pointer
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+unordered_map<Node*,Node*> mp;
+    Node* copyRandomList(Node* head) {
+        if(head==NULL) return NULL;
+        if(mp.count(head)) return mp[head];
+        Node* newNode = new Node(head->val);
+        mp[head] = newNode;
+        newNode->next = copyRandomList(head->next);
+        newNode->random = copyRandomList(head->random);
+        return newNode;
+    }
+};
