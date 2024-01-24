5c6f18f8e5d238452e1e94bfa6d26f378dd6ce37
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-24 21:05:40 +0530

    Update 24_jan.cpp

--- Daily/24_jan.cpp
+++ Daily/24_jan.cpp
@@ -207,3 +207,87 @@ int main()
 
 
 // } Driver Code Ends
+
+
+// Fractional Knapsack
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Item{
+    int value;
+    int weight;
+};
+
+
+// } Driver Code Ends
+//class implemented
+/*
+struct Item{
+    int value;
+    int weight;
+};
+*/
+
+
+class Solution
+{
+    public:
+    static bool cmp(pair<double,Item> i1,pair<double,Item> i2){
+        return i1.first > i2.first;
+    }
+    //Function to get the maximum total value in the knapsack.
+    double fractionalKnapsack(int W, Item arr[], int n)
+    {
+        vector<pair<double,Item>> v;
+        for(int i=0;i<n;i++){
+            double perUnit = (1.0*arr[i].value)/arr[i].weight;
+            pair<double,Item> p=make_pair(perUnit,arr[i]);
+            v.push_back(p);
+        }
+        sort(v.begin(),v.end(),cmp);
+        
+        double w=0,profit=0;
+        for(int i=0;i<n;i++){
+            if(v[i].second.weight>W){
+                profit+=W*v[i].first;
+                W=0;
+            }
+            else{
+                profit+=v[i].second.value;
+                W-=v[i].second.weight;
+            }
+        }
+        return profit;
+    }
+        
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	//taking testcases
+	cin>>t;
+	cout<<setprecision(6)<<fixed;
+	while(t--){
+	    //size of array and weight
+		int n, W;
+		cin>>n>>W;
+		
+		Item arr[n];
+		//value and weight of each item
+		for(int i=0;i<n;i++){
+			cin>>arr[i].value>>arr[i].weight;
+		}
+		
+		//function call
+		Solution ob;
+		cout<<ob.fractionalKnapsack(W, arr, n)<<endl;
+	}
+    return 0;
+}
+// } Driver Code Ends
