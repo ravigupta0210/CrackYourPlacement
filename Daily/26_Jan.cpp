44454e9860de0867ca0e8ab69752ccf69d92673c
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-26 12:19:51 +0530

    Update 26_Jan.cpp

--- Daily/26_Jan.cpp
+++ Daily/26_Jan.cpp
@@ -75,3 +75,38 @@ void solve(vector<string> &board,vector<vector<string>> &ans,int n,int col){
         return ans;
     }
 };
+
+
+// Sudoku Solver
+
+class Solution {
+public:
+bool isSafe(int row,int col,int k,vector<vector<char>>& board){
+    for(int i=0;i<9;i++){
+        if(board[row][i] == k+'0' || board[i][col] == k+'0' || board[3*(row/3)+i/3][3*(col/3)+i%3] == k+'0')
+        return false;
+    }
+    return true;
+}
+bool solve(vector<vector<char>>& board){
+    for(int row=0;row<9;row++){
+        for(int col=0;col<9;col++){
+            if(board[row][col]=='.'){
+                for(int k=1;k<=9;k++){
+                    if(isSafe(row,col,k,board)){
+                        board[row][col]=k+'0';
+                        bool ans = solve(board);
+                        if(ans) return true;
+                        else board[row][col]='.';
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};
