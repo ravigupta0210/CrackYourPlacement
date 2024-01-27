b1e00555f12462d0e0c9a87fdc6424328e2ba75c
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-27 22:04:52 +0530

    Update 27_Jan.cpp

--- Daily/27_Jan.cpp
+++ Daily/27_Jan.cpp
@@ -64,3 +64,79 @@ int main() {
 }
 
 // } Driver Code Ends
+
+
+// Rat in a Maze Problem - I
+
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution{
+    public:
+    bool isSafe(int n,vector<vector<int>> &m,vector<vector<bool>> &v,int x,int y){
+        if(x>=0 && x<n && y>=0 && y<n && v[x][y]!=1 && m[x][y]==1) return true;
+        return false;
+    }
+    void solve(vector<vector<bool>> &v,int n,vector<vector<int>> &m,int x,int y,string path,vector<string> &ans){
+        if(x==n-1 && y==n-1){
+            ans.push_back(path);
+            return ;
+        }
+        v[x][y]=1;
+        if(isSafe(n,m,v,x+1,y))
+        solve(v,n,m,x+1,y,path+'D',ans);
+        if(isSafe(n,m,v,x-1,y))
+        solve(v,n,m,x-1,y,path+'U',ans);
+        if(isSafe(n,m,v,x,y+1))
+        solve(v,n,m,x,y+1,path+'R',ans);
+        if(isSafe(n,m,v,x,y-1))
+        solve(v,n,m,x,y-1,path+'L',ans);
+        v[x][y]=0;
+    }
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        vector<string> ans;
+        if(m[0][0]==0) return ans;
+        string path="";
+        vector<vector<bool>> v(n,vector<bool>(n,0));
+        solve(v,n,m,0,0,path,ans);
+        return ans;
+    }
+};
+
+    
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> m(n, vector<int> (n,0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> m[i][j];
+            }
+        }
+        Solution obj;
+        vector<string> result = obj.findPath(m, n);
+        sort(result.begin(), result.end());
+        if (result.size() == 0)
+            cout << -1;
+        else
+            for (int i = 0; i < result.size(); i++) cout << result[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
