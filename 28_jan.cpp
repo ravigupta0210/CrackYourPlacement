f44f5cda7aad86d59231e7c7a9fe91100ffe1a0f
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-28 12:37:29 +0530

    Update 28_jan.cpp

--- 28_jan.cpp
+++ 28_jan.cpp
@@ -112,3 +112,22 @@ int Solution::findMedian(vector<vector<int> > &A) {
     }
     return mini;
 }
+
+
+// Single Element in a Sorted Array
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int left=0,right=nums.size()-1;
+        while(left<right){
+            int mid=(left+right)/2;
+            if(mid%2 == 1)
+            mid--;
+            if(nums[mid]!=nums[mid+1])
+            right=mid;
+            else left=mid+2;
+        }
+        return nums[left];
+    }
+};
