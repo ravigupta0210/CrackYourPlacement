8e86e21b133fafdd44fdee7eaca6268fc7250a02
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-28 12:39:39 +0530

    Update 28_jan.cpp

--- 28_jan.cpp
+++ 28_jan.cpp
@@ -258,3 +258,81 @@ int Solution::books(vector<int> &A, int B) {
 }
 
 // } Driver Code Ends
+
+
+
+// Aggressive Cows
+
+
+
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+    bool check(vector<int> &arr,int mid,int k){
+        int count=1,last=arr[0];
+        for(int i=1;i<arr.size();i++){
+            if(arr[i]-last >= mid)
+            {
+                count++;
+                last=arr[i];
+            }
+            if(count==k)
+            return true;
+        }
+        return false;
+    }
+    int solve(int n, int k, vector<int> &stalls) {
+    
+        sort(stalls.begin(),stalls.end());
+        int low=0,high=stalls[n-1]-stalls[0];
+        while(low<high){
+            int mid=(low+high+1)/2;
+            if(check(stalls,mid,k))
+            low=mid;
+            else
+            high=mid-1;
+        }
+        return low;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> stalls(n);
+        for (int i = 0; i < n; i++) {
+            cin >> stalls[i];
+        }
+        // char ch;
+        // cin >> ch;
+
+        Solution obj;
+        cout << obj.solve(n, k, stalls) << endl;
+
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
