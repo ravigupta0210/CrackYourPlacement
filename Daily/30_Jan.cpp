22c11daf2ed3fae98c647a05e047694a74aae344
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-30 12:11:27 +0530

    Update 30_Jan.cpp

--- Daily/30_Jan.cpp
+++ Daily/30_Jan.cpp
@@ -116,3 +116,49 @@ priority_queue<int,vector<int>,greater<int>> right;
  * obj->addNum(num);
  * double param_2 = obj->findMedian();
  */
+
+
+
+//  Merge K Sorted Arrays
+
+
+#include <bits/stdc++.h> 
+class node{
+    public:
+    int data;
+    int i;
+    int j;
+    node(int d,int r,int c){
+        this->data=d;
+        i=r;
+        j=c;
+    }
+};
+class compare{
+    public:
+    bool operator()(node* a,node* b){
+        return a->data > b->data;
+    }
+};
+vector<int> mergeKSortedArrays(vector<vector<int>>&kArrays, int k)
+{
+    priority_queue<node*,vector<node*>,compare> mini;
+    for(int i=0;i<k;i++){
+        node* temp=new node(kArrays[i][0],i,0);
+        mini.push(temp);
+    }
+    vector<int> ans;
+    while(mini.size()>0){
+        node* temp=mini.top();
+        ans.push_back(temp->data);
+        mini.pop();
+        int i=temp->i;
+        int j=temp->j;
+
+        if(j+1<kArrays[i].size()){
+            node* next = new node(kArrays[i][j+1],i,j+1);
+            mini.push(next);
+        }
+    }
+    return ans;
+}
