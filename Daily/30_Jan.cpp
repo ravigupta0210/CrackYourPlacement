1d13add7927f926d866766fa844c988881296fba
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-30 12:10:16 +0530

    Update 30_Jan.cpp

--- Daily/30_Jan.cpp
+++ Daily/30_Jan.cpp
@@ -14,3 +14,26 @@ vector<int> minHeap(int n, vector<vector<int>>& q) {
     }
     return ans;
 }
+
+
+// Kth Largest Element in an Array
+
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> maxheap;
+        for(auto i:nums){
+            if(maxheap.size()<(nums.size()-k+1)){
+                maxheap.push(i);
+            }
+            else{
+                if(maxheap.top()>i){
+                    maxheap.pop();
+                    maxheap.push(i);
+                }
+            }
+        }
+        return maxheap.top();
+    }
+};
