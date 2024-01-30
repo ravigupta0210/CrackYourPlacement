7e284e92d684f36ddd5ad31cdd42c8a0bdc0f286
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-30 12:10:40 +0530

    Update 30_Jan.cpp

--- Daily/30_Jan.cpp
+++ Daily/30_Jan.cpp
@@ -37,3 +37,34 @@ public:
         return maxheap.top();
     }
 };
+
+
+// Maximum Sum Combinations
+
+
+vector<int> Solution::solve(vector<int> &A, vector<int> &B, int C) {
+    priority_queue<int,vector<int>,greater<int>> minheap;
+    sort(A.begin(),A.end(),greater<int>());
+    sort(B.begin(),B.end(),greater<int>());
+    vector<int> ans(C);
+    
+    for(int i=0;i<C;i++){
+        minheap.push(A[i]+B[i]);
+    }
+    
+    for(int i=0;i<A.size();i++){
+        for(int j=0;j<B.size();j++){
+            if(i==j) continue;
+            if(A[i]+B[j] > minheap.top()){
+                minheap.pop();
+                minheap.push(A[i]+B[j]);
+            }
+            else break;
+        }
+    }
+    for(int i=C-1;i>=0;i--){
+        ans[i]=minheap.top();
+        minheap.pop();
+    }
+    return ans;
+}
