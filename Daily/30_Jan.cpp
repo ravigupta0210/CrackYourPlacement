91f7687d01fdd54ea58363e813fd8383e3d455ba
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-30 12:11:01 +0530

    Update 30_Jan.cpp

--- Daily/30_Jan.cpp
+++ Daily/30_Jan.cpp
@@ -68,3 +68,51 @@ vector<int> Solution::solve(vector<int> &A, vector<int> &B, int C) {
     }
     return ans;
 }
+
+
+// Find Median from Data Stream
+
+
+class MedianFinder {
+public:
+int n;
+double median;
+priority_queue<int> left;
+priority_queue<int,vector<int>,greater<int>> right;
+    MedianFinder() {
+        n=0;
+    }
+    
+    void addNum(int num) {
+        if(!left.empty() && left.top()>num){
+            left.push(num);
+            if(left.size() > right.size()+1){
+                right.push(left.top());
+                left.pop();
+            }
+        }
+        else{
+            right.push(num);
+            if(right.size()>left.size()+1){
+                left.push(right.top());
+                right.pop();
+            }
+        }
+        n++;
+    }
+    
+    double findMedian() {
+        if(n&1) {median=left.size()>right.size()?left.top():right.top();}
+        else{
+            median=(left.top()+right.top())/2.0;
+        }
+        return median;
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
