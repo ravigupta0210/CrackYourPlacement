75bde0bfb8fdf3a89597da1f5dd3f6495ffe00e4
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-31 12:17:12 +0530

    Update 31_Jan.cpp

--- Daily/31_Jan.cpp
+++ Daily/31_Jan.cpp
@@ -168,3 +168,26 @@ public:
         else return true;
     }
 };
+
+
+// Next Greater Element II
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans(n,-1);
+        stack<int> st;
+        for(int i=2*n-1;i>=0;i--){
+            while(!st.empty() && st.top()<=nums[i%n])
+            st.pop();
+
+            if(i<n){
+                if(!st.empty())
+                ans[i] = st.top();
+            }
+            st.push(nums[i%n]);
+        }
+        return ans;
+    }
+};
