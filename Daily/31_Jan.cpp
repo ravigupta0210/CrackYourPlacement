731e89f472bbd5fd8757bed9937d247e12d4ee98
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-01-31 12:16:10 +0530

    Update 31_Jan.cpp

--- Daily/31_Jan.cpp
+++ Daily/31_Jan.cpp
@@ -45,3 +45,57 @@ public:
     }
     
 };
+
+
+// Implement a Queue
+
+class Queue {
+ 
+public:
+int *arr;
+int rear;
+int qfront;
+int size;
+    Queue() {
+        size=100001;
+        arr = new int[size];
+        qfront=0;
+        rear=0;
+    }
+
+
+    bool isEmpty() {
+        if(qfront==rear) return true;
+        else return false;
+    }
+
+    void enqueue(int data) {
+        if(rear==size){
+            cout<<"Queue is Full"<<endl;
+        }
+        else{
+            arr[rear] = data;
+            rear++;
+        }
+    }
+
+    int dequeue() {
+        if(qfront==rear) return -1;
+        else{
+            int ans=arr[qfront];
+            arr[qfront]=-1;
+            qfront++;
+            if(rear==qfront){
+                qfront=0;
+                rear=0;
+            }
+            return ans;
+        }
+    }
+
+    int front() {
+        if(rear==qfront) return -1;
+        else
+        return arr[qfront];
+    }
+};
