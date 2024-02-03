8c5affc30173ca6262e4094b4f3ba9978c6f1b60
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-03 13:08:38 +0530

    Update 03_Feb.cpp

--- Daily/03_Feb.cpp
+++ Daily/03_Feb.cpp
@@ -62,3 +62,54 @@ vector<pair<int,int>> vec;
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
  */
+
+
+
+// Rotting Oranges
+
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+
+        int dx[]={-1,1,0,0};
+        int dy[]={0,0,-1,1};
+        queue<pair<int,int>> q;
+        int row=grid.size(),col = grid[0].size(),ans=0;
+
+        bool check=true;
+        for(int i=0;i<row;i++)
+        for(int j=0;j<col;j++)
+        if(grid[i][j]==1 || grid[i][j]==2) check = false;
+
+        if(check) return 0;
+
+        for(int i=0;i<row;i++)
+        for(int j=0;j<col;j++)
+        if(grid[i][j]==2)
+        q.push({i,j});
+
+        while(!q.empty()){
+            int size=q.size();
+            ans++;
+            
+            while(size--){
+                int i=q.front().first;
+                int j=q.front().second;
+                q.pop();
+                for(int index=0;index<4;index++){
+                    int x=i+dx[index];
+                    int y=j+dy[index];
+                    if(x>=0 && x<row && y>=0 && y<col && grid[x][y]==1){
+                        grid[x][y]=2;
+                        q.push({x,y});
+                    }
+                }
+            }
+        }
+        for(int i=0;i<row;i++)
+        for(int j=0;j<col;j++)
+        if(grid[i][j]==1) return -1;
+        
+        return ans-1;
+    }
+};
