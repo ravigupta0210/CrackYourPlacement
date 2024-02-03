0ef2b2e453c0405d5174f0a70cc84cde87e7ef4c
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-03 13:08:16 +0530

    Update 03_Feb.cpp

--- Daily/03_Feb.cpp
+++ Daily/03_Feb.cpp
@@ -20,3 +20,45 @@ public:
         return ans;
     }
 };
+
+
+// Min Stack
+
+
+class MinStack {
+public:
+vector<pair<int,int>> vec;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        pair<int,int> p;
+        p.first = val;
+        if(vec.size()==0)
+        p.second = val;
+        else p.second = min(val,vec.back().second);
+        vec.push_back(p);
+    }
+    
+    void pop() {
+        vec.pop_back();
+    }
+    
+    int top() {
+        return vec.back().first;
+    }
+    
+    int getMin() {
+        return vec.back().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
