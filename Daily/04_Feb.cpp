cad4d251aab374f6683edfdb191daccc056aa4da
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-04 10:50:13 +0530

    Update 04_Feb.cpp

--- Daily/04_Feb.cpp
+++ Daily/04_Feb.cpp
@@ -71,3 +71,22 @@ vector<int> maxMinWindow(vector<int> arr, int n) {
         }
         return ans;
 }
+
+
+// Sliding Window Maximum
+
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        deque<int> dq;
+        for(int i=0;i<nums.size();i++){
+            while(!dq.empty() && dq.front()==i-k) dq.pop_front();
+            while(!dq.empty() && nums[dq.back()] <= nums[i]) dq.pop_back();
+            dq.push_back(i);
+            if(i>=k-1) ans.push_back(nums[dq.front()]);
+        }
+        return ans;
+    }
+};
