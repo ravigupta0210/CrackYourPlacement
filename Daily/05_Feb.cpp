96f2586e50adcaa92063ef28099915ce93843d87
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-05 10:55:57 +0530

    Update 05_Feb.cpp

--- Daily/05_Feb.cpp
+++ Daily/05_Feb.cpp
@@ -57,3 +57,24 @@ bool solve(string &s,int i,int j){
         return ans;
     }
 };
+
+
+
+// Roman to Integer
+
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char,int> mp={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+        int n=s.length(),ans=0;
+        for(int i=0;i<n;i++){
+            if(i<n && mp[s[i]] < mp[s[i+1]]){
+                ans+=mp[s[i+1]]-mp[s[i]];
+                i++;
+            }
+            else ans+=mp[s[i]];
+        }
+        return ans;
+    }
+};
