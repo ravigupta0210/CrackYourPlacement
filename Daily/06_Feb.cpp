98b76fd7107405e504af7e7af6faec03942bad14
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-06 10:24:51 +0530

    Update 06_Feb.cpp

--- Daily/06_Feb.cpp
+++ Daily/06_Feb.cpp
@@ -32,3 +32,24 @@ pi[i]=j;
 }
 return A.size()-pi[n-1];
 }
+
+
+
+// Valid Anagram
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()) return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        
+        for(int i=0;i<s.length();i++){
+            if(s[i]!=t[i])
+            return false;
+        }
+        return true;
+
+    }
+};
