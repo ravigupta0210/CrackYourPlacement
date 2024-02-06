fbd3aa174a5e1e211218bbdf17342cc8b3b19151
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-06 10:26:56 +0530

    Update 06_Feb.cpp

--- Daily/06_Feb.cpp
+++ Daily/06_Feb.cpp
@@ -86,3 +86,34 @@ string solve(string s){
        return s;
     }
 };
+
+
+
+// Compare Version Numbers
+
+
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int ans1=0,ans2=0,i=0,j=0,n=version1.length(),m=version2.length();
+        while(i<n || j<m){
+            while(i<n && version1[i]!='.'){
+                ans1*=10;
+                ans1+=(version1[i]-'0');
+                i++;
+            }
+            while(j<m && version2[j]!='.'){
+                ans2*=10;
+                ans2+=(version2[j]-'0');
+                j++;
+            }
+            if(ans1<ans2) return -1;
+            if(ans1>ans2) return 1;
+            i++;
+            j++;
+            ans1=0;
+            ans2=0;
+        }
+        return 0;
+    }
+};
