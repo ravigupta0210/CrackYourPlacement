1f951d29f506b4027711e2105c86696cf32e0adf
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-06 10:24:30 +0530

    Update 06_Feb.cpp

--- Daily/06_Feb.cpp
+++ Daily/06_Feb.cpp
@@ -9,3 +9,26 @@ public:
         return found;
     }
 };
+
+
+// Minimum Characters required to make a String Palindromic
+
+
+int Solution::solve(string A) {
+    string s=A;
+reverse(A.begin(),A.end());
+s=s+'#'+A; //to avoid cases where full string is equal
+int n =s.size();
+vector<int>pi(n,0);
+for(int i =1;i<n;i++){
+int j =pi[i-1];
+while(j>0&&s[i]!=s[j]){
+j=pi[j-1];
+}
+if(s[i]==s[j]){
+j++;
+}
+pi[i]=j;
+}
+return A.size()-pi[n-1];
+}
