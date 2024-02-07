30068cf6aa70ac7cc81060b2a92c21d7b1fcd1ee
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-07 11:33:54 +0530

    Update 07_Feb.cpp

--- Daily/07_Feb.cpp
+++ Daily/07_Feb.cpp
@@ -595,3 +595,51 @@ vector<vector<int>> getTreeTraversal(TreeNode *root){
     ans.push_back(post);
     return ans;
 }
+
+
+// Vertical Order Traversal of a Binary Tree
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int,map<int,multiset<int>>> mp;
+        queue<pair<TreeNode*,pair<int,int>>> q;
+        q.push({root,{0,0}});
+        vector<vector<int>> ans;
+        while(!q.empty()){
+            auto front=q.front();
+            q.pop();
+            TreeNode* frontNode = front.first;
+            int hd=front.second.first;
+            int level=front.second.second;
+
+            mp[hd][level].insert(frontNode->val);
+
+            if(frontNode->left)
+            q.push({frontNode->left,{hd-1,level+1}});
+            if(frontNode->right)
+            q.push({frontNode->right,{hd+1,level+1}});
+        }
+        for(auto i:mp){
+            vector<int> temp;
+            for(auto j:i.second){
+                for(auto k:j.second)
+                temp.push_back(k);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
