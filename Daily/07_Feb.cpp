747a50fc5d71a39daa41b587dd88f57802e569d7
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-07 11:34:43 +0530

    Update 07_Feb.cpp

--- Daily/07_Feb.cpp
+++ Daily/07_Feb.cpp
@@ -679,3 +679,47 @@ vector<int> Solution::solve(TreeNode* root, int B) {
     }
     return {};
 }
+
+
+
+// Maximum Width of Binary Tree
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        if(root==NULL) return 0;
+        int ans=1;
+        queue<pair<TreeNode*,long long int>> q;
+        q.push({root,0});
+        while(!q.empty()){
+           int level=q.size();
+           int start=q.front().second;
+           int end=q.back().second;
+           ans=max(ans,end-start+1);
+           for(int i=0;i<level;i++){
+               auto front=q.front();
+               TreeNode* frontNode=front.first;
+               int index=front.second-start;
+               q.pop();
+               if(frontNode->left)
+               q.push({frontNode->left,2LL*index+1});
+               if(frontNode->right)
+               q.push({frontNode->right,2LL*index+2});
+           }
+
+        }
+        return ans;
+    }
+};
