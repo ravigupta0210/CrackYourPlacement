85c644aabe483f20030b2a69a07510d02ef6508f
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-08 11:17:21 +0530

    Update 08_Feb.cpp

--- Daily/08_Feb.cpp
+++ Daily/08_Feb.cpp
@@ -105,3 +105,35 @@ int solve(TreeNode* root,int &ans){
         return ans;
     }
 };
+
+
+// Balanced Binary Tree
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int solve(TreeNode* root){
+    if(root==NULL) return 0;
+    int left=solve(root->left);
+    int right=solve(root->right);
+    return max(left,right)+1;
+}
+    bool isBalanced(TreeNode* root) {
+        if(root==NULL) return true;
+        bool left=isBalanced(root->left);
+        bool right=isBalanced(root->right);
+        bool diff =abs(solve(root->left)-solve(root->right)) <= 1;
+        return left && right && diff;
+    }
+};
