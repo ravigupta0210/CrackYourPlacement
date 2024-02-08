8ba8937d7a3ebde10f0b84d5f2c407e5c4328fc7
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-08 11:16:42 +0530

    Update 08_Feb.cpp

--- Daily/08_Feb.cpp
+++ Daily/08_Feb.cpp
@@ -35,3 +35,42 @@ public:
         return ans;
     }
 };
+
+
+// Maximum Depth of Binary Tree
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        queue<TreeNode*> q;
+       
+        if(root==NULL) return 0;
+        int ans=0;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            
+            for(int i=0;i<size;i++){
+                TreeNode* frontNode=q.front();
+                q.pop();
+                if(frontNode->left) q.push(frontNode->left);
+                if(frontNode->right) q.push(frontNode->right);
+                
+            }
+            ans++;
+        }
+        return ans;
+    }
+};
