a102aeaa89e88230aca19c5a7cf813aa3c6b03cc
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-08 11:17:00 +0530

    Update 08_Feb.cpp

--- Daily/08_Feb.cpp
+++ Daily/08_Feb.cpp
@@ -74,3 +74,34 @@ public:
         return ans;
     }
 };
+
+
+// Diameter of Binary Tree
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int solve(TreeNode* root,int &ans){
+    if(root==NULL) return 0;
+    int left=solve(root->left,ans);
+    int right=solve(root->right,ans);
+    ans=max(ans,left+right);
+    return 1+max(left,right);
+}
+    int diameterOfBinaryTree(TreeNode* root) {
+        int ans=0;
+        solve(root,ans);
+        return ans;
+    }
+};
