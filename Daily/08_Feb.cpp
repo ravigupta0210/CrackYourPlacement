45e70c3de54924a2eb4f6b94772316bf240ed3c6
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-08 11:17:43 +0530

    Update 08_Feb.cpp

--- Daily/08_Feb.cpp
+++ Daily/08_Feb.cpp
@@ -137,3 +137,32 @@ int solve(TreeNode* root){
         return left && right && diff;
     }
 };
+
+
+
+// Lowest Common Ancestor of a Binary Tree
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL) return NULL;
+        if(root->val == p->val || root->val==q->val ) return root;
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+        if(left!=NULL && right!=NULL) return root;
+        else if(left==NULL && right!=NULL) return right;
+        else if(left!=NULL && right==NULL) return left;
+        else return NULL;
+
+    }
+};
