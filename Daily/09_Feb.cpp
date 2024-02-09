25baecfbb2f6e8e37263a26b3d18b07a807ff8ed
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-09 11:25:02 +0530

    Update 09_Feb.cpp

--- Daily/09_Feb.cpp
+++ Daily/09_Feb.cpp
@@ -27,3 +27,42 @@ int solve(TreeNode* root,int &ans){
         return ans;
     }
 };
+
+
+
+// Construct Binary Tree from Preorder and Inorder Traversal
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+TreeNode* solve(vector<int>& inorder, vector<int>& preorder,
+int start,int end,int &index,unordered_map<int,int> &mp, int n){
+    if(index>=n || start>end) return NULL;
+    int element=preorder[index++];
+    TreeNode* root=new TreeNode(element);
+    int pos=mp[element];
+    root->left=solve(inorder,preorder,start,pos-1,index,mp,n);
+    root->right=solve(inorder,preorder,pos+1,end,index,mp,n);
+    return root;
+}
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n=preorder.size();
+        int index=0;
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++)
+        mp[inorder[i]]=i;
+        return solve(inorder,preorder,0,n-1,index,mp,n);
+
+    }
+};
