deae82a119d18ce0a3626ddd16284d727734b0c0
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-09 11:25:46 +0530

    Update 09_Feb.cpp

--- Daily/09_Feb.cpp
+++ Daily/09_Feb.cpp
@@ -104,3 +104,33 @@ unordered_map<int,int> &mp){
         return solve(inorder,postorder,0,n-1,index,n,mp);
     }
 };
+
+
+// Symmetric Tree
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+bool solve(TreeNode* root1,TreeNode* root2){
+    if(root1==NULL && root2==NULL) return true;
+    else if(root1==NULL && root2!=NULL) return false;
+    else if(root1!=NULL && root2==NULL) return false;
+    if(root1->val!=root2->val) return false;
+    return solve(root1->left,root2->right) && solve(root1->right,root2->left);
+}
+    bool isSymmetric(TreeNode* root) {
+        if(root==NULL) return true;
+        return solve(root->left,root->right);
+    }
+};
