57142e1b86a44e50a81fc89e1a41ab8dd7d00030
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-09 11:27:17 +0530

    Update 09_Feb.cpp

--- Daily/09_Feb.cpp
+++ Daily/09_Feb.cpp
@@ -325,3 +325,56 @@ int main() {
     return 0;
 }
 // } Driver Code Ends
+
+
+
+//  Children Sum Property
+
+
+
+#include <bits/stdc++.h> 
+/*************************************************************
+
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode
+    {
+    public :
+        T data;
+        BinaryTreeNode < T > *left;
+        BinaryTreeNode < T > *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+void changeTree(BinaryTreeNode < int > * root) {
+    if(root==NULL) return ;
+    int child=0;
+    if(root->left) child+=root->left->data;
+    if(root->right) child+=root->right->data;
+    if(child<root->data){
+        if(root->left){
+                root->left->data+=root->data-child;
+        }
+        else if(root->right){
+                root->right->data+=root->data-child;
+        }
+    }
+    else{
+        root->data=child;
+    }
+    changeTree(root->left);
+    changeTree(root->right);
+
+    if(root->left!=NULL || root->right!=NULL){
+        int total=0;
+        if(root->left)total+=root->left->data;
+        if(root->right)total+=root->right->data;
+        root->data=total;
+    }
+}  
