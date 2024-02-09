e21143b7c4dff9fc20b8c295e61199770e5e8531
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-09 11:26:10 +0530

    Update 09_Feb.cpp

--- Daily/09_Feb.cpp
+++ Daily/09_Feb.cpp
@@ -134,3 +134,41 @@ bool solve(TreeNode* root1,TreeNode* root2){
         return solve(root->left,root->right);
     }
 };
+
+
+
+// Flatten Binary Tree to Linked List
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+TreeNode* solve(TreeNode* root){
+    root=root->left;
+    while(root->right!=NULL)
+    root=root->right;
+    return root;
+}
+    void flatten(TreeNode* root) {
+        TreeNode* curr=root;
+        while(curr!=NULL){
+            if(curr->left){
+                TreeNode* pred=solve(curr);
+                pred->right=curr->right;
+                curr->right=curr->left;
+                curr->left=NULL;
+            }
+            curr=curr->right;
+        }
+    }
+};
