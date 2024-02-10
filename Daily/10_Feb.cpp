6ba08ac0360f6d8e7256bf385a82b01e7c1175d4
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-10 12:15:38 +0530

    Update 10_Feb.cpp

--- Daily/10_Feb.cpp
+++ Daily/10_Feb.cpp
@@ -103,3 +103,36 @@ public:
         return root;
     }
 };
+
+
+
+// Construct Binary Search Tree from Preorder Traversal
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+TreeNode* solve(vector<int>& preorder,int maxi,int &index){
+    if(index>=preorder.size()) return NULL;
+    if(maxi<preorder[index]) return NULL;
+
+    TreeNode* root=new TreeNode(preorder[index++]);
+    root->left=solve(preorder,root->val,index);
+    root->right=solve(preorder,maxi,index);
+    return root;
+}
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int index=0;
+        return solve(preorder,INT_MAX,index);
+    }
+};
