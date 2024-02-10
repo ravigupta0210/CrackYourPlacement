c0bb6e29594abd66ed6a24ae8be6fae5785a6c7c
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-10 12:15:58 +0530

    Update 10_Feb.cpp

--- Daily/10_Feb.cpp
+++ Daily/10_Feb.cpp
@@ -136,3 +136,34 @@ TreeNode* solve(vector<int>& preorder,int maxi,int &index){
         return solve(preorder,INT_MAX,index);
     }
 };
+
+
+
+// Validate Binary Search Tree
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+bool solve(TreeNode* root,long long mini,long long maxi){
+    if(root== NULL) return true;
+    if(root->val>mini && root->val<maxi){
+        return (solve(root->left,mini,root->val) && solve(root->right,root->val,maxi));
+    }
+    else return false;
+}
+    bool isValidBST(TreeNode* root) {
+        long long int mini=-1000000000000,maxi=1000000000000;
+        return solve(root,mini,maxi);
+    }
+};
