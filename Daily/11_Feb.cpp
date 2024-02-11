e65dd83dda2f80f3a66eb0847e2de186bfe8bda9
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-11 11:23:35 +0530

    Update 11_Feb.cpp

--- Daily/11_Feb.cpp
+++ Daily/11_Feb.cpp
@@ -37,3 +37,51 @@ int floorInBST(TreeNode<int> * root, int X)
     }
     return ans;
 }
+
+
+//  Ceil from BST
+
+
+
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the TreeNode class structure:
+
+    class BinaryTreeNode {
+    public:
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+        
+        BinaryTreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+        
+        ~BinaryTreeNode() {
+            if (left) {
+              delete left;
+            }
+            if (right) {
+              delete right;
+            }
+        }
+    };
+
+************************************************************/
+
+int findCeil(BinaryTreeNode<int> *root, int x){
+    int ans=-1;
+    while(root){
+        if(root->data==x) return root->data;
+        else if(root->data<x)
+        root=root->right;
+        else{
+            ans=root->data;
+            root=root->left;
+        }
+    }
+    return ans;
+}
