7f628bf795f3ac3456f5b5e16348d2b2a57a0ead
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-11 11:24:41 +0530

    Update 11_Feb.cpp

--- Daily/11_Feb.cpp
+++ Daily/11_Feb.cpp
@@ -257,3 +257,44 @@ int main()
     return 1;
 }
 // } Driver Code Ends
+
+
+
+// Two Sum IV - Input is a BST
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+void solve(TreeNode* root,vector<int> &in){
+    if(root==NULL) return ;
+    solve(root->left,in);
+    in.push_back(root->val);
+    solve(root->right,in);
+}
+    bool findTarget(TreeNode* root, int k) {
+        vector<int> in;
+        solve(root,in);
+        int i=0,j=in.size()-1;
+        while(i<j){
+            int temp=in[i]+in[j];
+            if(temp==k)
+            return true;
+            else if(temp<k){
+                i++;
+            }
+            else j--;
+        }
+        return false;
+    }
+};
