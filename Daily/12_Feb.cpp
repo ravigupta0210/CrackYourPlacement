c48c8d57222a65a7bb6d7b175e8713eb4017604c
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-12 10:37:06 +0530

    Update 12_Feb.cpp

--- Daily/12_Feb.cpp
+++ Daily/12_Feb.cpp
@@ -33,3 +33,50 @@ TreeNode* solve(TreeNode* root){
         }
     }
 };
+
+
+// Find Median from Data Stream
+
+class MedianFinder {
+public:
+int n;
+double median;
+priority_queue<int> left;
+priority_queue<int,vector<int>,greater<int>> right;
+    MedianFinder() {
+        n=0;
+    }
+    
+    void addNum(int num) {
+        if(!left.empty() && left.top()>num){
+            left.push(num);
+            if(left.size()>right.size()+1){
+                right.push(left.top());
+                left.pop();
+            }
+        }
+        else{
+            right.push(num);
+            if(right.size()>left.size()+1){
+                left.push(right.top());
+                right.pop();
+            }
+        }
+        n++;
+    }
+    
+    double findMedian() {
+        if(n&1)
+        {if(left.size()>right.size())
+        return left.top();
+        else return right.top();}
+        else return (left.top()+right.top())/2.0;
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
