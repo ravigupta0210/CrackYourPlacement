342991ec322511be0218ae6ef2927de6e82c6786
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-14 19:58:10 +0530

    Update 14_Feb.cpp

--- Daily/14_Feb.cpp
+++ Daily/14_Feb.cpp
@@ -96,3 +96,45 @@ int detectCycleInDirectedGraph(int v, vector < pair < int, int >> & edges) {
     }
     return !(count==v);
 }
+
+
+
+//  Topological Sort
+
+
+
+#include <bits/stdc++.h> 
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+    unordered_map<int,list<int>> adj;
+    for(int i=0;i<edges.size();i++){
+        int u=edges[i][0];
+        int v=edges[i][1];
+        adj[u].push_back(v);
+    }
+    queue<int> q;
+    vector<int> indegree(v);
+
+    for(auto i:adj){
+        for(auto j:i.second){
+            indegree[j]++;
+        }
+    }
+
+    for(int i=0;i<v;i++){
+        if(indegree[i]==0)
+        q.push(i);
+    }
+    vector<int> ans;
+    while(!q.empty()){
+        int front=q.front();
+        q.pop();
+        ans.push_back(front);
+
+        for(auto i:adj[front]){
+            indegree[i]--;
+            if(indegree[i]==0)
+            q.push(i);
+        }
+    }
+    return ans;
+}
