15567a2d5c4000a6905127758275d851839fee24
Ravi Gupta <102231974+ravigupta0210@users.noreply.github.com>
2024-02-17 12:01:45 +0530

    Update 17_Feb.cpp

--- Daily/17_Feb.cpp
+++ Daily/17_Feb.cpp
@@ -93,3 +93,53 @@ int minimumSpanningTree(vector<vector<int>>& edges, int n)
   }
   return ans;
 }
+
+
+
+//  Bridges In A Graph
+
+
+
+#include<bits/stdc++.h>
+
+void dfs(int node,int parent,vector<int> &disc,vector<int> &low,unordered_map<int,bool> &vis,
+unordered_map<int,list<int>> &adj,vector<vector<int>> &result,int &timer){
+    vis[node]=true;
+    disc[node]=low[node]=timer++;
+    for(auto nbr:adj[node]){
+        if(nbr==parent) continue;
+        if(!vis[nbr]){
+            dfs(nbr,node,disc,low,vis,adj,result,timer);
+            low[node]=min(low[node],low[nbr]);
+            if(low[nbr]>disc[node]){
+                vector<int> ans;
+                ans.push_back(node);
+                ans.push_back(nbr);
+                result.push_back(ans);
+            }
+        }
+        else{
+            low[node]=min(low[node],disc[nbr]);
+        }
+    }
+}
+vector<vector<int>> findBridges(vector<vector<int>> &edges, int v, int e) {
+    unordered_map<int,list<int>> adj;
+    for(int i=0;i<edges.size();i++){
+        int u=edges[i][0];
+        int v=edges[i][1];
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    vector<int> disc(v,-1),low(v,-1);
+    int parent=-1;
+    unordered_map<int,bool> vis;
+
+    vector<vector<int>> result;
+    int timer=0;
+    for(int i=0;i<v;i++){
+        if(!vis[i])
+            dfs(i,parent,disc,low,vis,adj,result,timer);
+    }
+    return result;
+}
